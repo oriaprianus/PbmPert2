39bcc265fdc18710ed05014187f2b0f9413b469a
oriaprianus <oriaprianus@gmail.com>
2024-02-17 12:31:32 +0700

    Initial commit

---
+++ app/src/main/java/ac/id/utdi/aplikasijetpack/MainActivity.kt
@@ -0,0 +1,11 @@
+package ac.id.utdi.aplikasijetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
